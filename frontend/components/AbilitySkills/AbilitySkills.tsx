df53445d66f10fd1baa98d3511a27a1931c28fc5
Christopher McLean <mclean.webdev@gmail.com>
2022-07-30 04:15:04 -0600

    cleaned up ability skills table

--- frontend/components/AbilitySkills/AbilitySkills.tsx
+++ frontend/components/AbilitySkills/AbilitySkills.tsx
@@ -50,9 +50,30 @@ const AbilitySkills = ({
     survivalProf
   } = entity;
   
+  const profs = ['exp', 'prof'];
+
   const getProficientClass = (prof: string): string | undefined => {
-    const profs = ['exp', 'prof'];
     return profs.includes(prof) ? 'ability-skills-proficient' : undefined
+  };
+
+  const getProfString = (prof: string): string => {
+    return profs.includes(prof) ? prof : '';
+  };
+
+  const getRow = (
+    ability: string,
+    skill: string,
+    prof: string,
+    mod: number
+  ) => {
+    return (
+      <tr>
+        <td>{getProfString(prof)}</td>
+        <td>{ability}</td>
+        <td>{skill}</td>
+        <td className={getProficientClass(prof)}>{mod}</td>
+      </tr>
+    )
   };
 
   return (
@@ -66,114 +87,24 @@ const AbilitySkills = ({
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>{acrobaticsProf}</td>
-          <td>Dex</td>
-          <td>Acrobatics</td>
-          <td className={getProficientClass(acrobaticsProf)}>{acrobaticsMod}</td>
-        </tr>
-        <tr>
-          <td>{animalHandlingProf}</td>
-          <td>Wis</td>
-          <td>Animal Handling</td>
-          <td className={getProficientClass(animalHandlingProf)}>{animalHandlingMod}</td>
-        </tr>
-        <tr>
-          <td>{arcanaProf}</td>
-          <td>Int</td>
-          <td>Arcana</td>
-          <td className={getProficientClass(arcanaProf)}>{arcanaMod}</td>
-        </tr>
-        <tr>
-          <td>{athleticsProf}</td>
-          <td>Str</td>
-          <td>Athletics</td>
-          <td className={getProficientClass(athleticsProf)}>{athleticsMod}</td>
-        </tr>
-        <tr>
-          <td>{deceptionProf}</td>
-          <td>Cha</td>
-          <td>Deception</td>
-          <td className={getProficientClass(deceptionProf)}>{deceptionMod}</td>
-        </tr>
-        <tr>
-          <td>{historyProf}</td>
-          <td>Int</td>
-          <td>History</td>
-          <td className={getProficientClass(historyProf)}>{historyMod}</td>
-        </tr>
-        <tr>
-          <td>{insightProf}</td>
-          <td>Wis</td>
-          <td>Insight</td>
-          <td className={getProficientClass(insightProf)}>{insightMod}</td>
-        </tr>
-        <tr>
-          <td>{intimidationProf}</td>
-          <td>Cha</td>
-          <td>Intimidation</td>
-          <td className={getProficientClass(intimidationProf)}>{intimidationMod}</td>
-        </tr>
-        <tr>
-          <td>{investigationProf}</td>
-          <td>Int</td>
-          <td>Investigation</td>
-          <td className={getProficientClass(investigationProf)}>{investigationMod}</td>
-        </tr>
-        <tr>
-          <td>{medicineProf}</td>
-          <td>Wis</td>
-          <td>Medicine</td>
-          <td className={getProficientClass(medicineProf)}>{medicineMod}</td>
-        </tr>
-        <tr>
-          <td>{natureProf}</td>
-          <td>Int</td>
-          <td>Nature</td>
-          <td className={getProficientClass(natureProf)}>{natureMod}</td>
-        </tr>
-        <tr>
-          <td>{perceptionProf}</td>
-          <td>Wis</td>
-          <td>Perception</td>
-          <td className={getProficientClass(perceptionProf)}>{perceptionMod}</td>
-        </tr>
-        <tr>
-          <td>{performanceProf}</td>
-          <td>Cha</td>
-          <td>Performance</td>
-          <td className={getProficientClass(performanceProf)}>{performanceMod}</td>
-        </tr>
-        <tr>
-          <td>{perceptionProf}</td>
-          <td>Cha</td>
-          <td>Persuasion</td>
-          <td className={getProficientClass(persuasionProf)}>{persuasionMod}</td>
-        </tr>
-        <tr>
-          <td>{religionProf}</td>
-          <td>int</td>
-          <td>Religion</td>
-          <td className={getProficientClass(religionProf)}>{religionMod}</td>
-        </tr>
-        <tr>
-          <td>{sleightOfHandProf}</td>
-          <td>Dex</td>
-          <td>Sleight of Hand</td>
-          <td className={getProficientClass(sleightOfHandProf)}>{sleightOfHandMod}</td>
-        </tr>
-        <tr>
-          <td>{stealthProf}</td>
-          <td>Dex</td>
-          <td>Stealth</td>
-          <td className={getProficientClass(stealthProf)}>{stealthMod}</td>
-        </tr>
-        <tr>
-          <td>{survivalProf}</td>
-          <td>Wis</td>
-          <td>Survival</td>
-          <td className={getProficientClass(survivalProf)}>{survivalMod}</td>
-        </tr>
+        {getRow('Dex', 'Acrobatics', acrobaticsProf, acrobaticsMod)}
+        {getRow('Wis', 'Animal Handling', animalHandlingProf, animalHandlingMod)}
+        {getRow('Int', 'Arcana', arcanaProf, arcanaMod)}
+        {getRow('Str', 'Athletics', athleticsProf, athleticsMod)}
+        {getRow('Cha', 'Deception', deceptionProf, deceptionMod)}
+        {getRow('Int', 'History', historyProf, historyMod)}
+        {getRow('Wis', 'Insight', insightProf, insightMod)}
+        {getRow('Cha', 'Intimidation', intimidationProf, intimidationMod)}
+        {getRow('Int', 'Investigation', investigationProf, investigationMod)}
+        {getRow('Wis', 'Medicine', medicineProf, medicineMod)}
+        {getRow('Int', 'Nature', natureProf, natureMod)}
+        {getRow('Wis', 'Perception', perceptionProf, perceptionMod)}
+        {getRow('Cha', 'Performance', performanceProf, performanceMod)}
+        {getRow('Cha', 'Persuasion', persuasionProf, persuasionMod)}
+        {getRow('int', 'Religion', religionProf, religionMod)}
+        {getRow('Dex', 'Sleight of Hand', sleightOfHandProf, sleightOfHandMod)}
+        {getRow('Dex', 'Stealth', stealthProf, stealthMod)}
+        {getRow('Wis', 'Survival', survivalProf, survivalMod)}
       </tbody>
     </table>
   );
